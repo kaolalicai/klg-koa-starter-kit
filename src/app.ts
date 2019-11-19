2ae3886bfce8c0e31304dc68879f9ef4f8ef0b57
Nick <myfjdthink@163.com>
2019-11-19 17:43:13 +0800

    feat:akajs update

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,7 @@
-import {MongoModelBuilder} from '@akajs/mongoose'
+import {initMongoose} from '@akajs/mongoose'
 import {Application} from '@akajs/core'
-// 必须导入，注意顺序，后期考虑自动化引入方案
-import './model'
-new MongoModelBuilder().build()
 import './ioc.config'
-import './controller'
 
+initMongoose()
 const app: Application = new Application({})
 export {app}
